ad4e942fbaaadf0c59454acf0394e7a330bb723a
Brendon DSouza <brendonmufc@gmail.com>
2022-07-08 12:17:11 -0400

    edit title

--- src/App.js
+++ src/App.js
@@ -76,7 +76,7 @@ function App() {
   <section className="section-center">
     <form className='grocery-form' onSubmit={handleSubmit}>
       {alert.show && <Alert {...alert} removeAlert={showAlert} list={list} />}
-      <h3>Grocery bud</h3>
+      <h3>Grocery buddy</h3>
       <div className="form-control">
         <input 
           type="text" 
